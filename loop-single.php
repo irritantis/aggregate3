7f267f32e3589e436b06af48eea3c17ee49e7824
Satoshi Iritani <iri@loftwork.com>
2016-06-06 10:30:36 +0900

    テーマのコアいじってるやつ

--- loop-single.php
+++ loop-single.php
@@ -7,30 +7,11 @@
 	<?php } ?>
 
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-		<div class="entry post clearfix">
+		<div class="entry post clearfix loopsingle">
 			<?php if (get_option('aggregate_integration_single_top') <> '' && get_option('aggregate_integrate_singletop_enable') == 'on') echo(get_option('aggregate_integration_single_top')); ?>
 
 			<h1 class="title"><?php the_title(); ?></h1>
 			<?php get_template_part('includes/postinfo','single'); ?>
-
-			<?php if (get_option('aggregate_thumbnails') == 'on') { ?>
-				<?php
-					$thumb = '';
-					$width = 200;
-					$height = 200;
-					$classtext = 'single-thumb';
-					$titletext = get_the_title();
-					$thumbnail = get_thumbnail($width,$height,$classtext,$titletext,$titletext,false,'Entry');
-					$thumb = $thumbnail["thumb"];
-				?>
-
-				<?php if($thumb <> '') { ?>
-					<div class="thumb">
-						<?php print_thumbnail($thumb, $thumbnail["use_timthumb"], $titletext, $width, $height, $classtext); ?>
-						<span class="overlay"></span>
-					</div> 	<!-- end .thumb -->
-				<?php } ?>
-			<?php } ?>
 
 			<?php the_content(); ?>
 			<?php wp_link_pages(array('before' => '<p><strong>'.esc_html__('Pages','Aggregate').':</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
